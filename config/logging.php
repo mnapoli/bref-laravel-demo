877dd54ad0cdf6c17b7cb1e087051ff095186cb6
Matthieu Napoli <matthieu@mnapoli.fr>
2018-05-25 01:22:21 +0200

    Revert, we'll use environment variables instead

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['stderr'],
+            'channels' => ['single'],
         ],
 
         'single' => [
