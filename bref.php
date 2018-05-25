3775960f78537733b24d37de1e5377c98c6d8d23
Matthieu Napoli <matthieu@mnapoli.fr>
2018-05-25 10:38:21 +0200

    Auto-create the storage directory on lambdas
    Laravel does not create that directory automatically so we have to create it

--- bref.php
+++ bref.php
@@ -6,9 +6,14 @@ require __DIR__.'/vendor/autoload.php';
 
 $app = require_once __DIR__.'/bootstrap/app.php';
 
+// Laravel does not create that directory automatically so we have to create it
+if (!is_dir(storage_path('framework/views'))) {
+    if (!mkdir(storage_path('framework/views'), 0755, true)) {
+        die('Cannot create directory ' . storage_path('framework/views'));
+    }
+}
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-/** @var \Illuminate\Foundation\Console\Kernel $consoleKernel */
-$consoleKernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 
 $app = new \Bref\Application;
 $app->httpHandler(new Bref\Bridge\Laravel\LaravelAdapter($kernel));
